8a9ac9bdc01e85fc3b3fe77bb3c037005bf709e7
nanobot567 <567.nanobot@gmail.com>
2024-02-03 14:12:43 -0600

    Change formatting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -66,90 +66,92 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-  public XboxController controller = new XboxController(0);
-  public Drive drive = new Drive(this);
-  // Vision vision = new Vision();
+    public XboxController controller = new XboxController(0);
+    public Drive drive = new Drive(this);
+    // Vision vision = new Vision();
 
-  PIDController forwardController = new PIDController(0.9, 0, 0.0);
+    PIDController forwardController = new PIDController(0.9, 0, 0.0);
 
-  final double ANGULAR_P = 0.025;
-  final double ANGULAR_D = 0.0;
-  PIDController angleController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
+    final double ANGULAR_P = 0.025;
+    final double ANGULAR_D = 0.0;
+    PIDController angleController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
 
-  SlewRateLimiter filter = new SlewRateLimiter(0.5);
-  double previous_distance;
-  private final SendableChooser<String> chooser = new SendableChooser<>();
+    SlewRateLimiter filter = new SlewRateLimiter(0.5);
+    double previous_distance;
+    private final SendableChooser<String> chooser = new SendableChooser<>();
 
-  public Command getAutonomousCommand(String autoName) {
-    return new PathPlannerAuto(autoName);
-  }
-  public Command getAutoPath(String pathName) {
-    PathPlannerPath path = PathPlannerPath.fromPathFile(pathName);
-    return AutoBuilder.followPath(path);
-  }
-  
-  @Override
-  public void robotInit() {
-    chooser.addOption("Drive Forwards", "Drive Forwards");
-    chooser.addOption("Drive Backwards", "Drive Backwards");
-    SmartDashboard.putData("Path Chooser", chooser);
-  }
+    public Command getAutonomousCommand(String autoName) {
+        return new PathPlannerAuto(autoName);
+    }
 
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
+    public Command getAutoPath(String pathName) {
+        PathPlannerPath path = PathPlannerPath.fromPathFile(pathName);
+        return AutoBuilder.followPath(path);
+    }
 
-  @Override
-  public void autonomousInit() {
-    System.out.println("Scheduling Auto");
-    CommandScheduler.getInstance().cancelAll();
-    drive.zeroGyro();
-    drive.resetOdometry();
-    //CommandScheduler.getInstance().schedule(getAutonomousCommand("Drive Forwards"));
-    getAutonomousCommand((chooser.getSelected() != null) ? chooser.getSelected() : "Drive Forwards").schedule();
+    @Override
+    public void robotInit() {
+        chooser.addOption("Drive Forwards", "Drive Forwards");
+        chooser.addOption("Drive Backwards", "Drive Backwards");
+        SmartDashboard.putData("Path Chooser", chooser);
+    }
 
-  }
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
 
-  @Override
-  public void autonomousPeriodic() {
-  }
+    @Override
+    public void autonomousInit() {
+        System.out.println("Scheduling Auto");
+        CommandScheduler.getInstance().cancelAll();
+        drive.zeroGyro();
+        drive.resetOdometry();
+        // CommandScheduler.getInstance().schedule(getAutonomousCommand("Drive
+        // Forwards"));
+        getAutonomousCommand((chooser.getSelected() != null) ? chooser.getSelected() : "Drive Forwards").schedule();
 
-  @Override
-  public void teleopInit() {
-  }
+    }
 
-  @Override
-  public void teleopPeriodic() {
+    @Override
+    public void autonomousPeriodic() {
+    }
 
-    // if (vision.getPose2d().isPresent()) {
-    // drive.addVisionMeasurement(vision.getPose2d(), Timer.getFPGATimestamp());
-    // }
-    drive.periodic();
-  }
+    @Override
+    public void teleopInit() {
+    }
 
-  @Override
-  public void disabledInit() {
+    @Override
+    public void teleopPeriodic() {
 
-  }
+        // if (vision.getPose2d().isPresent()) {
+        // drive.addVisionMeasurement(vision.getPose2d(), Timer.getFPGATimestamp());
+        // }
+        drive.periodic();
+    }
 
-  @Override
-  public void disabledPeriodic() {
-  }
+    @Override
+    public void disabledInit() {
 
-  @Override
-  public void testInit() {
-  }
+    }
 
-  @Override
-  public void testPeriodic() {
-  }
+    @Override
+    public void disabledPeriodic() {
+    }
 
-  @Override
-  public void simulationInit() {
-  }
+    @Override
+    public void testInit() {
+    }
 
-  @Override
-  public void simulationPeriodic() {
-  }
+    @Override
+    public void testPeriodic() {
+    }
+
+    @Override
+    public void simulationInit() {
+    }
+
+    @Override
+    public void simulationPeriodic() {
+    }
 }
