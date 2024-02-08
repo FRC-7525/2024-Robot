f0121278efcb9847ebbf738754ed7f33df5f9317
Bailey Bonde <bondebaileyc@gmail.com>
2024-02-07 18:21:55 -0600

    Fixed values, and motor types
    Co-authored-by: Leif031809 <Leif031809@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,7 +1,8 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+// import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,22 +16,21 @@ enum ShootingStates {
 public class Shooter extends SubsystemBase {
     ShootingStates states = ShootingStates.OFF;
     Robot robot = null;
-    CANSparkMax shooterMotor1 = new CANSparkMax(1, MotorType.kBrushless);
-    CANSparkMax shooterMotor2 = new CANSparkMax(1, MotorType.kBrushless);
-    PIDController shootingController = new PIDController(1.5, 0, 0); // change p
+    private TalonFX shooterMotor1 = new TalonFX(1);
+    private TalonFX shooterMotor2 = new TalonFX(2);
+    PIDController shootingController = new PIDController(1.5, 0, 0); // tune p
 
     public Shooter(Robot robot) {
         this.robot = robot;
-        shooterMotor1.restoreFactoryDefaults();
-        shooterMotor2.restoreFactoryDefaults();
-        shooterMotor2.follow(shooterMotor1);
     }
 
     public void periodic() {
         if (states == states.OFF) {
             shooterMotor1.set(0);
+            shooterMotor2.set(0);
         } else if (states == states.SHOOTING) {
-            shooterMotor1.set(shootingController.calculate(shooterMotor1.get(), 1)); // change setpoint
+            shooterMotor1.set(shootingController.calculate(shooterMotor1.getVelocity().getValue(), 400));
+            shooterMotor2.set(shootingController.calculate(shooterMotor2.getVelocity().getValue(), 400));
 
         }
 
