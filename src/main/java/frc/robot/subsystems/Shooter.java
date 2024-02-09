048d0c9508dc87009d424966d1a074acc3528ba7
GreenTomato5 <grentomato@gmail.com>
2024-02-09 17:25:14 -0600

    Temp controller inputs

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 
@@ -12,6 +14,8 @@ enum ShootingStates {
 }
 
 public class Shooter extends SubsystemBase {
+    XboxController controller = new XboxController(1);
+
     ShootingStates states = ShootingStates.OFF;
     Robot robot = null;
     private TalonFX shooterMotor1 = new TalonFX(1);
@@ -24,12 +28,19 @@ public class Shooter extends SubsystemBase {
     }
 
     public void periodic() {
+        if (controller.getAButton() && states == ShootingStates.OFF) {
+            states = ShootingStates.SHOOTING;
+        } else if (controller.getAButton() && states == ShootingStates.SHOOTING) {
+            states = ShootingStates.OFF;
+        }
         if (states == ShootingStates.OFF) {
             shooterMotor1.set(0);
             shooterMotor2.set(0);
+            SmartDashboard.putString("Shooting States", "OFF");
         } else if (states == ShootingStates.SHOOTING) {
             shooterMotor1.set(shootingController.calculate(shooterMotor1.getVelocity().getValue(), 400));
             shooterMotor2.set(shootingController1.calculate(shooterMotor2.getVelocity().getValue(), 400));
+            SmartDashboard.putString("Shooting States", "SHOOTING");
         }
 
     }
