acf8f5857b30e8f12cef492c86e9a21a6b3bd808
GreenTomato5 <grentomato@gmail.com>
2024-02-09 15:51:30 -0600

    added driveGearRatio and path planner file to constants file.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,15 +1,17 @@
 package frc.robot;
 
 public final class Constants {
-  public static final double stickDeadband = 0.1; 
-  public static final class Drive {
-    /* Drive Constants */
-    public static final int wheelDiameter = 4;
-    public static final double neoDriveGearRatio = 6.12;
-    public static final double angleGearRatio = 21.4286;
-    public static final double encoderResolution = 42;
-    public static final double falconDriveGearRatio = 6.75;
-    public static final boolean isNeo = true; // SET TO FALSE FOR FALCON
-    public static final int leftXSign = isNeo ? -1 : 1; // Inverts the controllers leftX sign if we're using a Neo
-  }
+    public static final double stickDeadband = 0.1; 
+    public static final class Drive {
+        /* Drive Constants */
+        public static final int wheelDiameter = 4;
+        public static final double neoDriveGearRatio = 6.12;
+        public static final double angleGearRatio = 21.4286;
+        public static final double encoderResolution = 42;
+        public static final double falconDriveGearRatio = 6.75;
+        public static final boolean isNeo = true; // SET TO FALSE FOR FALCON
+        public static final int leftXSign = isNeo ? -1 : 1; // Inverts the controllers leftX sign if we're using a Neo
+        public static final String pathPlannerFile = isNeo ? "swerve/neo" : "swerve/falcon";
+        public static final double driveGearRatio = isNeo ? neoDriveGearRatio : falconDriveGearRatio;
+    }
 }
