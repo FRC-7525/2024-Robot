80cf6c884d3b89c4312af7af4dc9510816fbbd85
lucia <llcsaturnaliam@gmail.com>
2024-02-08 22:12:25 -0600

    created constants class + moved drive constants into there

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public final class Constants {
+  /* Drive Constants */
+  public static final double stickDeadband = 0.1;
+  public static final int wheelDiameter = 4;
+  public static final double neoDriveGearRatio = 6.12;
+  public static final double angleGearRatio = 21.4286;
+  public static final double encoderResolution = 42;
+  public static final double falconDriveGearRatio = 6.75;
+  public static final boolean isNeo = true; // SET TO FALSE FOR FALCON
+}
