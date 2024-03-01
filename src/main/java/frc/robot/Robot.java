bdcb1661470873af3e056f88f27ff00157a5d7d1
Otto <ottowatsonbrown@Icloud.com>
2024-03-01 17:45:18 -0600

    Correctly Configured Autos

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -76,7 +76,7 @@ public class Robot extends TimedRobot {
         chooser.addOption("Drive backwards, score preload", "Drive Backwards + Score");
         chooser.addOption("PID Tuning Auto", "PID Tuning Auto");
         //Choreo Autos
-        chooser.addOption("4 Note Choreo", "Optimized 4 Note");
+        chooser.addOption("2 Note Choreo", "Optimized 2 Note");
         // 2 Note Autos
         chooser.addOption("Preload + Left Note", "Left Note");
         chooser.addOption("Preload + Middle Note", "Middle Note");
