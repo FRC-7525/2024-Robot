6aa0c1c4c472d28cf66237771f1b022ecdaa0d5f
Liquid2112 <shubhpatel2504@gmail.com>
2024-03-13 23:49:48 -0500

    only amp and speaker tags needs to be adjusted

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -19,15 +19,25 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
-
-public class Vision { 
+public class Vision {
     Optional<EstimatedRobotPose> frontBotpose3d;
 
     PhotonCamera frontCamera = new PhotonCamera("Front Camera");
 
-    Transform3d frontrobotToCam = new Transform3d(new Translation3d(Units.inchesToMeters(-14.25), 0, Units.inchesToMeters(6)),
+    Transform3d frontrobotToCam = new Transform3d(
+            new Translation3d(Units.inchesToMeters(-14.25), 0, Units.inchesToMeters(6)),
             new Rotation3d(0, Units.degreesToRadians(-67), Units.degreesToRadians(180)));
-    PhotonPoseEstimator frontEstimator = new PhotonPoseEstimator(AprilTagFields.k2024Crescendo.loadAprilTagLayoutField(),
+
+    List<AprilTag> aprilTags = Arrays.asList(
+            new AprilTag(3, new Pose3d(8.308467, 0.877443, 1.451102, new Rotation3d(0.0, 0.0, -90.0))),
+            new AprilTag(4, new Pose3d(8.308467, 1.442593, 1.451102, new Rotation3d(0.0, 0.0, -90.0))),
+            new AprilTag(7, new Pose3d(-8.308975, 1.442593, 1.451102, new Rotation3d(0.0, 0.0, 90.0))),
+            new AprilTag(8, new Pose3d(-8.308975, 0.877443, 1.451102, new Rotation3d(0.0, 0.0, 90.0))),
+            new AprilTag(5, new Pose3d(6.429883, 4.098925, 1.355852, new Rotation3d(0.0, 0.0, -180.0))),
+            new AprilTag(6, new Pose3d(-6.429375, 4.098925, 1.355852, new Rotation3d(0.0, 0.0, 0.0))));
+    AprilTagFieldLayout layout = new AprilTagFieldLayout(aprilTags);
+
+    PhotonPoseEstimator frontEstimator = new PhotonPoseEstimator(layout,
             PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, frontCamera,
             frontrobotToCam);
     Boolean seesFrontVision = false;
@@ -35,10 +45,11 @@ public class Vision {
 
     public void periodic() {
         frontBotpose3d = frontEstimator.update();
-        SmartDashboard.putBoolean("Front Vision", seesFrontVision); 
+        SmartDashboard.putBoolean("Front Vision", seesFrontVision);
         if (frontBotpose3d.isPresent()) {
             var tempPose = frontBotpose3d.get().estimatedPose;
-            double[] frontPose = {tempPose.getX(), tempPose.getY(), Units.radiansToDegrees(tempPose.getRotation().getAngle())};
+            double[] frontPose = { tempPose.getX(), tempPose.getY(),
+                    Units.radiansToDegrees(tempPose.getRotation().getAngle()) };
 
             SmartDashboard.putNumberArray("Front Pose", frontPose);
         }
