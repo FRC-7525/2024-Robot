df7fa8df190c49542dc8dfe6fa6d85eafd48ab74
Otto <ottowatsonbrown@icloud.com>
2024-03-27 18:13:57 -0500

    added back side camera not tested

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -27,6 +27,7 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.apriltag.AprilTagPoseEstimate;
 
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
@@ -34,39 +35,60 @@ import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 public class Vision {
 
     Optional<EstimatedRobotPose> frontBotpose3d;
+    Optional<EstimatedRobotPose> sideBotpose3d;
 
     PhotonCamera frontCamera = new PhotonCamera("Front Camera");
+    PhotonCamera sideCamera = new PhotonCamera("Side Camera");
 
     Transform3d frontrobotToCam = new Transform3d(
             new Translation3d(Units.inchesToMeters(-14.25), 0, Units.inchesToMeters(6)),
             new Rotation3d(0, Units.degreesToRadians(-67), Units.degreesToRadians(180)));
 
+    Transform3d siderobotToCam = new Transform3d(
+            new Translation3d(Units.inchesToMeters(-9.25), Units.inchesToMeters(9.75), Units.inchesToMeters(15.25)),
+            new Rotation3d(0, Units.degreesToRadians(-9.5), Units.degreesToRadians(90)));
+
     AprilTagFieldLayout layout;
     PhotonPoseEstimator frontEstimator;
+    PhotonPoseEstimator sideEstimator;
 
     public Vision() {
         try {
             String deployDirectoryPath = Filesystem.getDeployDirectory().getAbsolutePath();
             layout = new AprilTagFieldLayout(deployDirectoryPath + "/CrescendoFieldLayout.json");
             frontEstimator = new PhotonPoseEstimator(layout,
-                PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, frontCamera,
-                frontrobotToCam);
+                    PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, frontCamera,
+                    frontrobotToCam);
+            sideEstimator = new PhotonPoseEstimator(layout, 
+                    PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, sideCamera,
+                    siderobotToCam);
         } catch (IOException e) {
             System.out.println(e);
         }
     }
 
     Boolean seesFrontVision = false;
+    Boolean seesSideVision = false;
     Timer frontVisionTimer = new Timer();
+    Timer sideVisionTimer = new Timer();
 
     public void periodic() {
         frontBotpose3d = frontEstimator.update();
+        sideBotpose3d = sideEstimator.update();
         SmartDashboard.putBoolean("Front Vision", seesFrontVision);
         if (frontBotpose3d.isPresent()) {
-            var tempPose = frontBotpose3d.get().estimatedPose;
-            double[] frontPose = {tempPose.getX(), tempPose.getY(), Units.radiansToDegrees(tempPose.getRotation().getZ())};
+            var frontTempPose = frontBotpose3d.get().estimatedPose;
+            double[] frontPose = { frontTempPose.getX(), frontTempPose.getY(),
+                    Units.radiansToDegrees(frontTempPose.getRotation().getZ()) };
 
             SmartDashboard.putNumberArray("Front Pose", frontPose);
+        }
+        if (sideBotpose3d.isPresent()) {
+            var sideTempPose = sideBotpose3d.get().estimatedPose;
+            double[] sidePose = { sideTempPose.getX(), sideTempPose.getY(),
+                    Units.radiansToDegrees(sideTempPose.getRotation().getZ()) };
+
+            SmartDashboard.putNumberArray("Side Pose", sidePose);
         }
     }
 
@@ -79,6 +101,20 @@ public class Vision {
         } else {
             if (frontVisionTimer.get() > Constants.Vision.LAST_VISION_MEASURMENT_TIMER) {
                 seesFrontVision = false;
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Pose2d> getSidePose2d() {
+        if (sideBotpose3d.isPresent()) {
+            seesSideVision = true;
+            sideVisionTimer.reset();
+            sideVisionTimer.start();
+            return Optional.of(sideBotpose3d.get().estimatedPose.toPose2d());
+        } else {
+            if (sideVisionTimer.get() > Constants.Vision.LAST_VISION_MEASURMENT_TIMER) {
+                seesSideVision = false;
             }
         }
         return Optional.empty();
